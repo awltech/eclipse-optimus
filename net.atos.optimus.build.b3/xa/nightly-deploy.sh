addc8a6af12f17cfea4acc55c27d7597482acb45
mvanbesien <mvaawl@gmail.com>
2013-10-31 09:31:49 +0100

    Added message

--- net.atos.optimus.build.b3/xa/nightly-deploy.sh
+++ net.atos.optimus.build.b3/xa/nightly-deploy.sh
@@ -6,7 +6,7 @@
 
 TEMPDIR=/DATA/xa/temp/optimus
 
-echo "Rebuilding update site structure..."
+echo "[NIGHTLY-DEPLOY] Rebuilding update site structure..."
 rm -rf ${TEMPDIR}/*
 mkdir ${TEMPDIR}/common
 mkdir ${TEMPDIR}/m2m-engine
@@ -17,7 +17,7 @@ mkdir ${TEMPDIR}/m2t-merger-java
 mkdir ${TEMPDIR}/m2t-selector
 mkdir ${TEMPDIR}/t2m-java
 
-echo "Moving all the update sites to a destination folder..."
+echo "[NIGHTLY-DEPLOY] Moving all the update sites to a destination folder..."
 cp -a "${WORKSPACE}/net.atos.optimus.common.parent/net.atos.optimus.common.site/target/repository/." ${TEMPDIR}/common
 cp -a "${WORKSPACE}/net.atos.optimus.m2m.engine.parent/net.atos.optimus.m2m.engine.site/target/repository/." ${TEMPDIR}/m2m-engine
 cp -a "${WORKSPACE}/net.atos.optimus.m2m.engine.sdk.parent/net.atos.optimus.m2m.engine.sdk.site/target/repository/." ${TEMPDIR}/m2m-engine-sdk
@@ -27,14 +27,16 @@ cp -a "${WORKSPACE}/net.atos.optimus.m2t.merger.java.parent/net.atos.optimus.m2t
 cp -a "${WORKSPACE}/net.atos.optimus.m2t.selector.parent/net.atos.optimus.m2t.selector.site/target/repository/." ${TEMPDIR}/m2t-selector
 cp -a "${WORKSPACE}/net.atos.optimus.t2m.java.parent/net.atos.optimus.t2m.java.site/target/repository/." ${TEMPDIR}/t2m-java
 
-echo "Invoking aggregation process..."
+echo "[NIGHTLY-DEPLOY] Invoking aggregation process..."
 /DATA/xa/CI/eclipseHeadlessB3/b3 aggregate --action BUILD --buildModel "${WORKSPACE}/net.atos.optimus.build.b3/xa/nightly-aggregation.b3aggr" --buildRoot ${TEMPDIR}/b3
 
-echo "Move the result of the aggregation..."
+echo "[NIGHTLY-DEPLOY] Move the result of the aggregation..."
 rm -rf /DATA/xa/indivXAPluginsUpdSite/net.atos.optimus/aggregated/*
 cp -a ${TEMPDIR}/b3/final/. /DATA/xa/indivXAPluginsUpdSite/net.atos.optimus/aggregated/
 rm -rf /DATA/xa/eclipse/nightly/optimus/*
 cp -a ${TEMPDIR}/b3/final/. /DATA/xa/eclipse/nightly/optimus/
 
-echo "Cleaning temporary folder..."
+echo "[NIGHTLY-DEPLOY] Cleaning temporary folder..."
 rm -rf ${TEMPDIR}/*
+
+echo "[NIGHTLY-DEPLOY] Done."
