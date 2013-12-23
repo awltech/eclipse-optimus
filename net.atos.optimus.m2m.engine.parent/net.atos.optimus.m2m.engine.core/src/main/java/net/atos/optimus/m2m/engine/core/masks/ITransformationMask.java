547f2549b5cd5a258559b57346307b0be6fffb7d
mvanbesien <mvaawl@gmail.com>
2013-12-23 16:05:40 +0100

    Documented

--- net.atos.optimus.m2m.engine.parent/net.atos.optimus.m2m.engine.core/src/main/java/net/atos/optimus/m2m/engine/core/masks/ITransformationMask.java
+++ net.atos.optimus.m2m.engine.parent/net.atos.optimus.m2m.engine.core/src/main/java/net/atos/optimus/m2m/engine/core/masks/ITransformationMask.java
@@ -1,7 +1,44 @@
+/**
+ * Optimus, framework for Model Transformation
+ *
+ * Copyright (C) 2013 Worldline or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package net.atos.optimus.m2m.engine.core.masks;
 
+/**
+ * This interface is for implementing a filtering process over the
+ * transformations execution. Here, the masks act as filters, to know if a
+ * transformation is enabled or not.
+ * 
+ * @author mvanbesien
+ * @since 1.0
+ * 
+ */
 public interface ITransformationMask {
 
+	/**
+	 * Returns true if the transformation, which ID is provided as parameter, is
+	 * enabled for this mask
+	 * 
+	 * @param id transformationId
+	 * @return
+	 */
 	public boolean isTransformationEnabled(String id);
-	
+
 }
--- net.atos.optimus.m2m.engine.parent/net.atos.optimus.m2m.engine.core/src/main/java/net/atos/optimus/m2m/engine/core/masks/PreferencesTransformationMask.java
+++ net.atos.optimus.m2m.engine.parent/net.atos.optimus.m2m.engine.core/src/main/java/net/atos/optimus/m2m/engine/core/masks/PreferencesTransformationMask.java
@@ -1,9 +1,40 @@
+/**
+ * Optimus, framework for Model Transformation
+ *
+ * Copyright (C) 2013 Worldline or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package net.atos.optimus.m2m.engine.core.masks;
 
 import net.atos.optimus.m2m.engine.core.Activator;
 
 import org.eclipse.jface.preference.IPreferenceStore;
 
+/**
+ * Implementation of Transformation mask for the user preferences.
+ * 
+ * Indeed, the user preferences for transformation execution are handled through
+ * a transformation mask
+ * 
+ * @author mvanbesien
+ * @since 1.0
+ * 
+ */
 public class PreferencesTransformationMask implements ITransformationMask {
 
 	/**
@@ -33,13 +64,30 @@ public class PreferencesTransformationMask implements ITransformationMask {
 	private PreferencesTransformationMask() {
 	}
 
+	/**
+	 * Internal instance of preferenceStore
+	 */
 	private IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.atos.optimus.m2m.engine.core.masks.ITransformationMask#
+	 * isTransformationEnabled(java.lang.String)
+	 */
+	@Override
 	public boolean isTransformationEnabled(String id) {
 		String enablementKey = Activator.PLUGIN_ID + ".disabled." + id;
 		return !this.preferenceStore.getBoolean(enablementKey);
 	}
 
+	/**
+	 * Updates the value for the enablement of the transformation, which id is passed as
+	 * parameter
+	 * 
+	 * @param id
+	 * @param enabled
+	 */
 	public void setTransformationEnabled(String id, boolean enabled) {
 		String enablementKey = Activator.PLUGIN_ID + ".disabled." + id;
 		this.preferenceStore.setValue(enablementKey, !enabled);
