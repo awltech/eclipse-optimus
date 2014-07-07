11cc5c6dc5c927842456482dc1ee62fa439982bc
mvanbesien <mvaawl@gmail.com>
2014-07-07 20:57:48 +0200

    Initial Revision

---
+++ net.atos.optimus.m2m.engine.parent/net.atos.optimus.m2m.engine.core/src/main/java/net/atos/optimus/m2m/engine/core/ctxinject/impl/ParentContextElementInjector.java
@@ -0,0 +1,84 @@
+/**
+ * Optimus, framework for Model Transformation
+ *
+ * Copyright (C) 2013 Worldline or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package net.atos.optimus.m2m.engine.core.ctxinject.impl;
+
+import java.lang.reflect.Field;
+
+import net.atos.optimus.m2m.engine.core.ctxinject.ContextElementVisibility;
+import net.atos.optimus.m2m.engine.core.ctxinject.ParentContextElement;
+import net.atos.optimus.m2m.engine.core.transformations.AbstractTransformation;
+import net.atos.optimus.m2m.engine.core.transformations.ITransformationContext;
+
+import org.eclipse.emf.ecore.EObject;
+
+public class ParentContextElementInjector extends Injector {
+
+	private Field field;
+	private ContextElementVisibility visibility;
+	private boolean nullable;
+	private String mapping;
+
+	public ParentContextElementInjector(Field field) {
+		this.field = field;
+		ParentContextElement annotation = field.getAnnotation(ParentContextElement.class);
+		this.visibility = annotation.visibility();
+		this.mapping = annotation.value();
+		this.nullable = annotation.nullable();
+	}
+
+	@Override
+	public void inject(AbstractTransformation<?> transformation, ITransformationContext context)
+			throws NullValueException, NullInstanceException, FieldInjectionException {
+		EObject parent = transformation.getEObject().eContainer();
+		if (this.isInjectable() && parent != null) {
+			EObject eObject = this.mapping != null ? context.get(parent, this.mapping) : null;
+			if (eObject == null && !this.nullable)
+				throw new NullValueException(field.getName());
+			setValue(transformation, field, eObject);
+		}
+	}
+
+	@Override
+	public void update(AbstractTransformation<?> transformation, ITransformationContext context)
+			throws NullValueException, NullInstanceException, FieldUpdateException {
+		EObject parent = transformation.getEObject().eContainer();
+		if (this.isUpdatable() && parent != null) {
+			Object object = getValue(transformation, field);
+			if (object == null && !this.nullable) {
+				throw new NullValueException(field.getName());
+			}
+			if (object instanceof EObject) {
+				context.put(parent, this.mapping, (EObject) object);
+			}
+		}
+
+	}
+
+	private boolean isInjectable() {
+		return this.visibility != null ? this.visibility.isInjectable() : false;
+	}
+
+	private boolean isUpdatable() {
+		return this.visibility != null ? this.visibility.isUpdatable() : false;
+	}
+
+}
