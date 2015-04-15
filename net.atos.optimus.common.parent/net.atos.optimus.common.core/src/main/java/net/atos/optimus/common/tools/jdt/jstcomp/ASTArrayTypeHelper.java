ce2cce7f9c71ef3805f5a87792eb05743e2ad416
[a125788] Maxence Vanbésien <maxence.vanbesien@worldline.com>
2015-04-15 17:47:42 +0200

    Added high level javadoc and header.

--- net.atos.optimus.common.parent/net.atos.optimus.common.core/src/main/java/net/atos/optimus/common/tools/jdt/jstcomp/ASTArrayTypeHelper.java
+++ net.atos.optimus.common.parent/net.atos.optimus.common.core/src/main/java/net/atos/optimus/common/tools/jdt/jstcomp/ASTArrayTypeHelper.java
@@ -1,11 +1,41 @@
 package net.atos.optimus.common.tools.jdt.jstcomp;
 
+/**
+ * Optimus, framework for Model Transformation
+ *
+ * Copyright (C) 2013 Worldline or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 import java.util.List;
 
 import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.ChildPropertyDescriptor;
 import org.eclipse.jdt.core.dom.Type;
 
+/**
+ * Implementation of helper that gives abstraction level over deprecated methods
+ * between AST4 & AST8, to prevent AST to throw exceptions and keep compliance
+ * with older eclipse versions.
+ * 
+ * @author mvanbesien
+ * @since 1.1
+ *
+ */
 public class ASTArrayTypeHelper {
 
 	private ASTArrayTypeHelper() {
@@ -37,7 +67,7 @@ public class ASTArrayTypeHelper {
 			if (structuralProperty instanceof Type) {
 				return (Type) structuralProperty;
 			}
-		} 
+		}
 		return null;
 	}
 }
