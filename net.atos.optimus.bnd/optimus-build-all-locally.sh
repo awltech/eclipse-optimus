3b1a823c0202305c99985d03fd24312e5209dbf9
mvanbesien <mvaawl@gmail.com>
2015-04-29 15:51:01 +0200

    Updated version to 1.2.0 to EMF Metamodel as well.

--- net.atos.optimus.bnd/optimus-build-all-locally.sh
+++ net.atos.optimus.bnd/optimus-build-all-locally.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
-
-mvn -f ../net.atos.optimus.parent/pom.xml install
-
-mvn -f ../net.atos.optimus.common.parent/pom.xml install
-
-mvn -f ../net.atos.optimus.emf.metamodels.parent/pom.xml install
-
-mvn -f ../net.atos.optimus.t2m.java.parent/pom.xml install
-
-mvn -f ../net.atos.optimus.m2m.engine.parent/pom.xml install
-mvn -f ../net.atos.optimus.m2m.javaxmi.parent/pom.xml install
-
-mvn -f ../net.atos.optimus.m2t.selector.parent/pom.xml install
-mvn -f ../net.atos.optimus.m2t.merger.java.parent/pom.xml install
-mvn -f ../net.atos.optimus.m2t.java.parent/pom.xml install
-
-mvn -f ../net.atos.optimus.m2m.engine.sdk.parent/pom.xml install
-
-
-
-
-
-
+echo "***************************************"
+echo "* Optimus Global Maven Builder Script *"
+echo "***************************************"
+mvn -f ../net.atos.optimus.parent/pom.xml install; 
+mvn -f ../net.atos.optimus.common.parent/pom.xml install;
+mvn -f ../net.atos.optimus.emf.metamodels.parent/pom.xml install;
+mvn -f ../net.atos.optimus.t2m.java.parent/pom.xml install;
+mvn -f ../net.atos.optimus.m2m.engine.parent/pom.xml install;
+mvn -f ../net.atos.optimus.m2m.javaxmi.parent/pom.xml install;
+mvn -f ../net.atos.optimus.m2t.selector.parent/pom.xml install;
+mvn -f ../net.atos.optimus.m2t.merger.java.parent/pom.xml install;
+mvn -f ../net.atos.optimus.m2t.java.parent/pom.xml install;
+mvn -f ../net.atos.optimus.m2m.engine.sdk.parent/pom.xml install;
