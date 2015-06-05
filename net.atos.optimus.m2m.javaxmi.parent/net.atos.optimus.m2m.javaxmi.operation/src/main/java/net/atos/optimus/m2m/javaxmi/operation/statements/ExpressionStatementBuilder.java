e16914ef5c987c5f46d31498fbfc65048eaa186e
tnachtergaele <nachtergaele.thomas@gmail.com>
2015-06-05 17:50:50 +0200

    Added builder dedicated to assignment and expression statement

---
+++ net.atos.optimus.m2m.javaxmi.parent/net.atos.optimus.m2m.javaxmi.operation/src/main/java/net/atos/optimus/m2m/javaxmi/operation/statements/ExpressionStatementBuilder.java
@@ -0,0 +1,79 @@
+/**
+ * Optimus, framework for Model Transformation
+ *
+ * Copyright (C) 2013 Worldline or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package net.atos.optimus.m2m.javaxmi.operation.statements;
+
+import org.eclipse.gmt.modisco.java.Expression;
+import org.eclipse.gmt.modisco.java.ExpressionStatement;
+import org.eclipse.gmt.modisco.java.emf.JavaFactory;
+
+/**
+ * A builder dedicated to create expression statement of modisco model
+ * 
+ * @author tnachtergaele <nachtergaele.thomas@gmail.com>
+ * 
+ *
+ */
+
+public class ExpressionStatementBuilder {
+
+	/** The build expression statement */
+	private ExpressionStatement buildExpressionStatement;
+
+	/**
+	 * Give a expression statement unit builder
+	 * 
+	 * @return a new expression statement builder.
+	 */
+	public static ExpressionStatementBuilder builder() {
+		return new ExpressionStatementBuilder();
+	}
+
+	/**
+	 * Private constructor
+	 * 
+	 */
+	private ExpressionStatementBuilder() {
+		this.buildExpressionStatement = JavaFactory.eINSTANCE.createExpressionStatement();
+	}
+
+	/**
+	 * Build a expression statement of modisco model
+	 * 
+	 * @return a new expression statement of modisco model.
+	 */
+	public ExpressionStatement build() {
+		return this.buildExpressionStatement;
+	}
+
+	/**
+	 * Set the expression of the expression statement under construction
+	 * 
+	 * @param expression
+	 *            the expression of the expression statement under construction.
+	 * @return the builder.
+	 */
+	public ExpressionStatementBuilder setExpression(Expression expression) {
+		this.buildExpressionStatement.setExpression(expression);
+		return this;
+	}
+
+}
